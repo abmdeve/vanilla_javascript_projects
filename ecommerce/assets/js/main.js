382550b2538fc46d06b47c40ee0d6e1a36059fb5
abmdeve <tundederome@gmail.com>
2025-03-10 12:03:16 +0100

    Home screen is implemented with responsive

--- ecommerce/assets/js/main.js
+++ ecommerce/assets/js/main.js
@@ -1,26 +1,66 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== SHOW CART ===============*/
+// APRES AVOIR IMPLEMENTER LE SCROLL
+const cart = document.getElementById("cart"),
+  cartShop = document.getElementById("cart-shop"),
+  cartClose = document.getElementById("cart-close");
 
 /*===== CART SHOW =====*/
 /* Validate if constant exists */
+if (cartShop) {
+  cartShop.addEventListener("click", () => {
+    cart.classList.add("show-cart");
+  });
+}
 
 /*===== CART HIDDEN =====*/
 /* Validate if constant exists */
+if (cartClose) {
+  cartClose.addEventListener("click", () => {
+    cart.classList.remove("show-cart");
+  });
+}
 
 /*=============== SHOW LOGIN ===============*/
+const login = document.getElementById("login"),
+  loginButton = document.getElementById("login-button"),
+  loginClose = document.getElementById("login-close");
 
 /*===== LOGIN SHOW =====*/
 /* Validate if constant exists */
+if (loginButton) {
+  loginButton.addEventListener("click", () => {
+    login.classList.remove("show-login");
+  });
+}
 
 /*===== LOGIN HIDDEN =====*/
 /* Validate if constant exists */
+if (loginClose) {
+  loginClose.addEventListener("click", () => {
+    login.classList.remove("show-login");
+  });
+}
 
 /*=============== HOME SWIPER ===============*/
 var homeSwiper = new Swiper(".home-swiper", {
@@ -51,6 +91,14 @@ var homeSwiper = new Swiper(".new-swiper", {
 });
 
 /*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to a tag with the scroll top
+  // class
+  if (scrollY >= 350) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
 
 /*=============== LIGHT BOX ===============*/
 
